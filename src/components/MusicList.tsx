93fb5f9e2ec27ed47fbdf55a955749467e30e353
ninefloor <less0805@gmail.com>
2025-01-29 18:12:38 +0900

    modify music list

--- src/components/MusicList.tsx
+++ src/components/MusicList.tsx
@@ -1,24 +1,31 @@
 import { Music } from "@appTypes/music";
 import { Button, Flex, Text } from "@radix-ui/themes";
 import { musics } from "@utils/music";
+import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled(Flex)``;
+const Container = styled(Flex)`
+  min-width: 320px;
+  height: calc(100dvh - 56px);
+  overflow: auto;
+`;
 
 export const MusicList = () => {
   return (
     <Container direction="column" gap="2">
       {musics.map((music) => (
-        <Item item={music} />
+        <Item item={music} key={music.id} />
       ))}
     </Container>
   );
 };
 
-const ItemContainer = styled(Button)`
+const ItemContainer = styled(Button)<{ $isCurrent: boolean }>`
   flex-direction: column;
   align-items: flex-start;
   margin: 0;
+  background-color: ${({ $isCurrent }) =>
+    $isCurrent ? `var(--accent-a3)` : `var(--accent-a0)`};
 `;
 
 interface ItemProps {
@@ -26,12 +33,27 @@ interface ItemProps {
 }
 
 const Item = ({ item }: ItemProps) => {
+  const navigate = useNavigate();
+  const { novelId } = useParams();
   return (
-    <ItemContainer variant="ghost">
-      <Text size="4" weight="medium">
-        {item.korTitle}
+    <ItemContainer
+      $isCurrent={novelId === String(item.id)}
+      variant="ghost"
+      onClick={() => navigate(`/${item.id}`)}
+    >
+      <Flex gap="2" align="end">
+        <Text size="4" weight="medium">
+          {item.korTitle}
+        </Text>
+      </Flex>
+      <Text size="1">
+        {item.title} / {item.enTitle}
       </Text>
-      <Text size="1">{item.title}</Text>
+      {item.novelTitle && (
+        <Text size="1">
+          {item.novelWriter} &lt;{item.novelTitle}&gt;
+        </Text>
+      )}
     </ItemContainer>
   );
 };
--- src/types/music.d.ts
+++ src/types/music.d.ts
@@ -1,6 +1,15 @@
-export interface Music {
+export interface MusicBase {
+  id: number;
   korTitle: string;
+  enTitle: string;
   title: string;
+  novelTitle: string;
+  novelWriter: string;
   translated: boolean;
   isPublished: boolean;
+  originalNovelUrl?: string;
 }
+
+export type Music =
+  | (MusicBase & { translated: true; translator: string })
+  | (MusicBase & { translated: false });
