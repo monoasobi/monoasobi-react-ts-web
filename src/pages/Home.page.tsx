008eecfba82e90510a9e5e558c3eb93bcec3db2b
ninefloor <less0805@gmail.com>
2025-05-11 14:13:27 +0900

    supabase 제거, 홈 업데이트

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -3,11 +3,13 @@ import yoasobi from "@assets/yoasobi.jpg";
 import { adminAtom } from "@atoms/admin.atom";
 import { Loading } from "@components/Loading";
 import { WrenchIcon } from "@heroicons/react/24/outline";
+import { ArrowDownIcon } from "@heroicons/react/24/solid";
 import {
   Button,
   Card,
   Dialog,
   Flex,
+  Heading,
   IconButton,
   Text,
   TextField,
@@ -127,7 +129,33 @@ export const Home = () => {
               번역자에게 있으며, 모노아소비는 이 자료들을 공유함으로써 어떠한
               상업적 이익도 추구하지 않습니다.
             </Text>
-            <Text size="2" align="center">
+            <Card>
+              <Heading as="h3" size="3">
+                주인장 추천 루트
+              </Heading>
+              <Flex direction="column" gap="2" align="center">
+                <Text size="2" align="center">
+                  소설을 읽는다
+                </Text>
+                <ArrowDownIcon width={12} />
+                <Text size="2" align="center">
+                  번역된 가사와 함께 노래를 들으며
+                  <br />
+                  아야세의 작사 실력에 감탄한다
+                </Text>
+                <ArrowDownIcon width={12} />
+                <Text size="2" align="center">
+                  뮤직비디오를 감상하며
+                  <br />
+                  소설에 등장한 디테일을 발견하고 감탄한다
+                </Text>
+                <ArrowDownIcon width={12} />
+                <Text size="2" align="center">
+                  입덕완료!
+                </Text>
+              </Flex>
+            </Card>
+            <Text size="2" align="center" weight="bold">
               번역 투고 및 기타 문의 :{" "}
               <a href="mailto:envi.9.official@gmail.com?subject=모노아소비 문의">
                 envi.9.official@gmail.com
